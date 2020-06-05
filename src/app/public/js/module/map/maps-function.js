8907357cb2e85942e9fa6d3d4ff3873066c58654
Enrico Augusto Rocha Ferreira <enricoferreira41@gmail.com>
2020-06-04 22:35:12 -0300

    arquitetura css, zoom, data-desc

--- src/app/public/js/module/map/maps-function.js
+++ src/app/public/js/module/map/maps-function.js
@@ -2,6 +2,6 @@
 export function renderMap(lat, lng) {     
     var currentLocale = {lat, lng};    
     var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 8, center: currentLocale});    
+        document.getElementById('map'), {zoom: 12, center: currentLocale});    
     var marker = new google.maps.Marker({position: currentLocale, map: map});
   }
