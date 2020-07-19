25f7f6dff25c1868c7818ec4f7b82a144d586ab9
enricoferreira <38507827+enricoferreira@users.noreply.github.com>
2020-07-19 07:57:55 -0300

    key api

--- src/app/public/js/urlKey.js
+++ src/app/public/js/urlKey.js
@@ -1,5 +1,6 @@
 export const searchLatLong = (cep) =>{        
-    return fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${cep}&key=AIzaSyDDlk6O71OM4rpzCV31colJ0CxzyM7ViZ8`)
+    const apiKey = '';
+    return fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${cep}&key=${apiKey}`)
     .then(data => data.json())
     .then(json => {
         const location = json.results[0].geometry.location;        
